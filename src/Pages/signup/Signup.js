18a6e85127bc656abe984f3f879a3aca86d27b3f
chaeahpark <parkchaeah331@gmail.com>
2022-01-10 16:14:21 +0200

    passing the auth context to signup form and set up the function to handle the form submit.

--- src/Pages/signup/Signup.js
+++ src/Pages/signup/Signup.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useAuth } from "../../contexts/AuthContext";
 
 const Signup = () => {
+  const { signUp } = useAuth();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //validation
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return;
+    }
+
+    try {
+      // 1. before sign up, sign up error should be an empty string.
+      // 2. dispatch and change the loading state to true
+      await signUp(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      // dispatch action type SIGN_UP_FAIL
+    }
+
+    // set loading state to FALSE
+  };
+
   return (
     <div className="form-page">
       <div className="form-container">
@@ -10,19 +35,19 @@ const Signup = () => {
             <label htmlFor="email" className="input-label">
               Email:
             </label>
-            <input type="text" placeholder="ex) abc@gmail.com" />
+            <input type="text" placeholder="ex) abc@gmail.com" ref={emailRef} />
           </div>
           <div className="input-container">
             <label htmlFor="password" className="input-label">
               Password:
             </label>
-            <input type="text" placeholder="" />
+            <input type="text" placeholder="" ref={passwordRef} />
           </div>
           <div className="input-container">
             <label htmlFor="passwordConfirmation" className="input-label">
               Password Confirmation:
             </label>
-            <input type="text" placeholder="" />
+            <input type="text" placeholder="" ref={passwordConfirmRef} />
           </div>
           <button className="form-btn" type="submit" value="submit">
             Sign Up
