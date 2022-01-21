d0c69a3c2c12e51d066adaae33240285f7678082
chaeahpark <parkchaeah331@gmail.com>
2022-01-21 18:01:02 +0200

    added querying and filtering data function on the tag sidebar.

--- src/Pages/Main/Sidebar.js
+++ src/Pages/Main/Sidebar.js
@@ -1,14 +1,31 @@
 import { useTag } from "../../contexts/TagContext";
 import uuid from "react-uuid";
 
+import { query, where, getDocs } from "firebase/firestore";
+import { postsCollection } from "../../api/postApi";
+
 const Sidebar = () => {
   const { options } = useTag();
+  const tagQuery = (option) => {
+    return query(postsCollection, where("tags", "array-contains", option));
+  };
+
+  const handleOnTagClick = async (option) => {
+    const dataSnapshot = await getDocs(tagQuery(option));
+    dataSnapshot.forEach((doc) => console.log(doc.id, " => ", doc.data()));
+  };
 
   const renderTags = options.map((option) => {
     return (
-      <p className="tag__item" key={uuid()}>
+      <li
+        className="tag__item"
+        key={uuid()}
+        onClick={() => {
+          handleOnTagClick(option);
+        }}
+      >
         #{option}
-      </p>
+      </li>
     );
   });
 
@@ -17,7 +34,7 @@ const Sidebar = () => {
       <div className="main__sidebar">
         <div className="sidebar__tags">
           <h3 className="main__title title--tag">Tags</h3>
-          {renderTags}
+          <ul>{renderTags}</ul>
         </div>
         <div className="sidebar__languages"></div>
       </div>
