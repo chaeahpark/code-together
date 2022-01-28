6320c5bc0a1d88a9e45c8f7a4fdb2ad50bdc88f7
chaeahpark <parkchaeah331@gmail.com>
2022-01-28 17:02:23 +0200

    Download a photoURL from firestore authentication and display on the MYPROFILE page.

--- src/Pages/myProfile/MyProfile.js
+++ src/Pages/myProfile/MyProfile.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
 import profileDefault from "../../assets/images/profileDefault.png";
 import { useAuth } from "../../contexts/AuthContext";
 
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getAuth, updateProfile } from "firebase/auth";
 
 const MyProfile = () => {
-  const { setProfileImageUrl, profileImageUrl } = useAuth();
+  const { user, setProfileImageUrl, profileImageUrl } = useAuth();
+  const authRef = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = () => {
+      if (authRef !== null) {
+        const userData = authRef.currentUser;
+        const { photoURL } = userData;
+        setProfileImageUrl(photoURL);
+      }
+    };
+
+    return unsubscribe;
+  }, []);
 
   const onFileChange = async (e) => {
     const file = e.target.files[0];
@@ -15,10 +29,14 @@ const MyProfile = () => {
 
     // update image file on firestore
     await uploadBytes(imagesRef, file);
-
+    // get image url from firestore
     const url = await getDownloadURL(imagesRef);
+    // update user data (user photo url) in firestore
+    await updateProfile(authRef.currentUser, {
+      photoURL: url,
+    });
+
     setProfileImageUrl(url);
-    console.log(profileImageUrl);
   };
 
   return (
@@ -29,7 +47,7 @@ const MyProfile = () => {
         <div className="myProfile-image">
           <div className="image-display">
             {/* add default image */}
-            <img className="image-updated" src={profileDefault} alt="" />
+            <img className="image-updated" src={profileImageUrl} alt="" />
             <div className="image-control">
               <label htmlFor="">
                 Choose image
